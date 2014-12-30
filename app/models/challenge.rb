2df64497115736def50643469237543a8bba7685
suman <sghosh968@gmail.com>
2014-12-31 00:52:52 +0530

    Added various methods to user & challenge models

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,16 @@
 class Challenge < ActiveRecord::Base
   resourcify
+
+
+
+
+  #get owners of a challenge
+  def get_owners
+    self.roles.where(:name => "owner").first.users
+  end
+
+  #to check if a user is an owner of a challenge or not
+  def is_owner?(user)
+    self.get_owners.include?(user)
+  end
 end
