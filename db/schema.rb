53c2d0560578b06a3e311f1bff69f19ae6b4ac63
suman <sghosh968@gmail.com>
2015-01-10 01:53:55 +0530

    Added devise confirmable yo user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108010619) do
+ActiveRecord::Schema.define(version: 20150109195411) do
 
   create_table "challenges", force: true do |t|
     t.string   "name"
@@ -58,8 +58,13 @@ ActiveRecord::Schema.define(version: 20150108010619) do
     t.string   "website"
     t.string   "facebook_url"
     t.string   "twitter_url"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
