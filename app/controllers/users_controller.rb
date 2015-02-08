b730d5f508896234c95712e1c71182d62003b4ce
suman <sghosh968@gmail.com>
2015-02-08 23:10:22 +0530

    Added action for dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :dashboard]
 
   def edit
 
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
         #format.json { render json: @challenge.errors, status: :unprocessable_entity }
       end
     end
+
+  end
+
+  def dashboard
 
   end
 
