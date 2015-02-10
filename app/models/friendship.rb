c38d23d8543ae73ce692298b2404483538c93274
suman <sghosh968@gmail.com>
2015-02-11 01:18:41 +0530

    scope to get user's received friend requests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,4 +4,6 @@ class Friendship < ActiveRecord::Base
 
   scope :confirmed, -> { where(status: "Accepted") }
   scope :unconfirmed, -> { where(:status => "RequestSent") }
+  scope :pending_received_friend_requests, ->(friend_id) { where(:friend_id => friend_id, :status => "RequestSent") }
+  #scope :pending_sent_friend_requests, -> { where(:status => "RequestSent") }
 end
