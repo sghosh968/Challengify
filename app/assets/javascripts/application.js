ca971fdb9caed944a622a42ee60faa77ec3b2de8
suman <sghosh968@gmail.com>
2015-02-12 23:16:10 +0530

    Changed theme to admin lte

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,95 +10,17 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-// require turbolinks
-
-//= require jquery-1.10.2.min
-//= require jquery-migrate-1.2.1.min
-//= require jquery-ui
-//= require bootstrap.min
-//= require bootstrap-hover-dropdown
-//= require html5shiv
-//= require respond.min
-//= require jquery.metisMenu
-//= require jquery.slimscroll
-//= require jquery.cookie
-//= require icheck.min
-//= require custom.min
-//= require jquery.news-ticker
-//= require jquery.menu
-//= require pace.min
-//= require holder
-//= require responsive-tabs
-//= require jquery.flot
-//= require jquery.flot.categories
-//= require jquery.flot.pie
-//= require jquery.flot.tooltip
-//= require jquery.flot.resize
-//= require jquery.flot.fillbetween
-//= require jquery.flot.stack
-//= require jquery.flot.spline
-//= require zabuto_calendar.min
-//= require index
-//= require highcharts
-//= require data
-//= require drilldown
-//= require exporting
-//= require highcharts-more
-//= require charts-highchart-pie
-//= require charts-highchart-more
-//= require custom
-//= require flipclock.min
-//= require main
 
 
-
-
-
-
-
-
-
-
-// require jquery.nestable
-
-
-
-
-
-
-
-// require animation
-
-// require challenges.js
-// require charts-chartjs
-// require charts-flotchart
-// require charts-highchart-area
-// require charts-highchart-column-bar
-// require charts-highchart-combinations
-// require charts-highchart-dynamic
-// require charts-highchart-line
-// require charts-highchart-more
-// require charts-highchart-scatter-bubble
-
-
-
-
-// require funnel
-
-
-
-
-// require jplist
-// require jplist.min
-
-
-// require modernizr.min
-
-
-
-// require ui-nestable-list
-// require users.js
-//= require jquery.datetimepicker
-//= require jquery.datetimepicker/init
+//= require app.min
+//= require plugins/sparkline/jquery.sparkline.min
+//= require plugins/jvectormap/jquery-jvectormap-1.2.2.min.js
+//= require plugins/jvectormap/jquery-jvectormap-world-mill-en.js
+//= require plugins/jqueryKnob/jquery.knob.js
+//= require plugins/daterangepicker/daterangepicker.js
+//= require plugins/datepicker/bootstrap-datepicker.js
+//= require plugins/iCheck/icheck.min.js
+//= require plugins/slimScroll/jquery.slimscroll.min.js
+//= require plugins/chartjs/Chart.min.js
+//= require pages/dashboard2.js
+//= require demo.js
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+
+  def get_user_image_url
+    current_user.image.blank? ? asset_path( 'avatar5.png' ) : current_user.image.remote_url
+  end
 end
