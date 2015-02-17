7445abef0f1e8d7083e28e3a751ae46a9f2332b7
suman <sghosh968@gmail.com>
2015-02-17 19:03:23 +0530

    Added helper method to return view for friend requests notification

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,20 @@ module UsersHelper
   def get_user_image_url(user)
     user.image.blank? ? asset_path( 'avatar5.png' ) : current_user.image.remote_url
   end
+
+  def friend_requests_notifications(freind_requests)
+    str = ""
+    freind_requests.each do |friend_request|
+      str +="<li>
+        <a href='#'>
+          <img src=#{ get_user_image_url(friend_request.user)} class='img-circle' alt='User Image' style='width: 25px;height: 25px;'/>&nbsp;&nbsp;
+          <span class='hidden-xs'>#{friend_request.user.name}</span>
+          <div class='pull-right'>
+          <button class='btn btn-success btn-xs'><i class='fa fa-fw fa-check-circle'></i>&nbsp;Accept</button>&nbsp;&nbsp;<button class='btn btn-danger btn-xs'><i class='fa fa-fw fa-times-circle'></i>&nbsp;Reject</button></div>
+          </br><span><small>#{time_ago_in_words(User.find(1).pending_received_friend_requests.first.created_at)}&nbsp;ago</small></span>
+        </a>
+      </li>".html_safe()
+    end
+    str
+  end
 end
