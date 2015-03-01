52f8670f9a3a635b4ba48fed8f7351f40827e80a
suman <sghosh968@gmail.com>
2015-03-01 16:26:44 +0530

    Added helper method to display friend request notifications on top nav bar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,15 +7,23 @@ module UsersHelper
   def friend_requests_notifications(freind_requests)
     str = ""
     freind_requests.each do |friend_request|
-      str +="<li>
+      str +="<form action=#{friendship_process_friendship_request_path(friend_request)} data-remote=true method='post'>
+      <fieldset id=>
+      <li>
         <a href='#'>
-          <img src=#{ get_user_image_url(friend_request.user)} class='img-circle' alt='User Image' style='width: 25px;height: 25px;'/>&nbsp;&nbsp;
+          <img src=#{ get_user_image_url(friend_request.user)} class='img-circle' alt='User Image' style='width: 25px;height: 25px;' />&nbsp;&nbsp;
+          <input type='hidden' name='process' id=#{friend_request.id}>
           <span class='hidden-xs'>#{friend_request.user.name}</span>
           <div class='pull-right'>
-          <button class='btn btn-success btn-xs'><i class='fa fa-fw fa-check-circle'></i>&nbsp;Accept</button>&nbsp;&nbsp;<button class='btn btn-danger btn-xs'><i class='fa fa-fw fa-times-circle'></i>&nbsp;Reject</button></div>
-          </br><span><small>#{time_ago_in_words(User.find(1).pending_received_friend_requests.first.created_at)}&nbsp;ago</small></span>
+            <button class='btn btn-success btn-xs fr_accept' value='accept_#{friend_request.id}'><i class='fa fa-fw fa-check-circle'></i>&nbsp;Accept</button>&nbsp;&nbsp;
+            <button class='btn btn-danger btn-xs fr_reject' value='reject_#{friend_request.id}' ><i class='fa fa-fw fa-times-circle'></i>&nbsp;Reject</button>
+          </div></br>
+          <span>
+            <small>#{time_ago_in_words(User.find(1).pending_received_friend_requests.first.created_at)}&nbsp;ago</small>
+          </span>
         </a>
-      </li>".html_safe()
+      </li></fieldset>
+      </form>".html_safe()
     end
     str
   end
